0f2db56ff049333f7dbff607f3ad71780de333ae
Pipeloluwa Adebayo <pipeloluwapraise@gmail.com>
2021-11-25 15:00:50 +0100

    initial commit

--- places/settings.py
+++ places/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_&$w^7byy#p+)9a^-^#e=kdr#s1&o)4kev5+jrx@*dcoj6$o(2'
+SECRET_KEY = ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['adebayopipeloluwa.pythonanywhere.com']
 
 
 # Application definition
